2ff6d78e2b47e7d83fffbe68719f235b027eb891
堀野康輔 <kosuke19990808@icloud.com>
2023-12-31 23:17:17 +0900

    Initial Commit

---
+++ SmartCode/SmartCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartCodeApp.swift
+//  SmartCode
+//
+//  Created by 堀野康輔 on 2023/12/31.
+//
+
+import SwiftUI
+
+@main
+struct SmartCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
